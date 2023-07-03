be09c2131ee7a4a4b88ee1d37e5345f617ecc06e
simon despres <simondprs62790@hotmail.fr>
2023-07-03 17:12:28 +0200

    made form login

--- components/UserAuthForm.vue
+++ components/UserAuthForm.vue
@@ -1,6 +1,5 @@
-<template>
+<!-- <template>
   <div>
-    <!-- Bootstrap Form  -->
     <form id="UserForm">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email:</label>
@@ -20,6 +19,64 @@
 </template>
 
 <script>
+</script>
+
+<style>
+</style> -->
+
+<template>
+  <v-main>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar color="blue">
+              <v-toolbar-title>Login form</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field name="email" label="Email" type="text" v-model="email"></v-text-field>
+                <v-text-field id="password" name="password" label="Password" type="password"
+                  v-model="password"></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="black" @click="loginHandler">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    async loginHandler() {
+      const data = { 'email': this.email, 'password': this.password }
+      console.log(data);
+      try {
+        const response = await this.$auth.loginWith('local', { data: data })
+        console.log(response)
+        this.$auth.$storage.setUniversal('email', response.data.email)
+        await this.$auth.setUserToken(response.data.access_token, response.data.refresh_token)
+
+      } catch (e) {
+        console.log(e.message)
+      }
+    }
+  }
+};
 </script>
 
 <style>
