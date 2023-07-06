09b00e8733bed79d4fa54ce838df02aedd7425c3
simon despres <simondprs62790@hotmail.fr>
2023-07-06 10:42:31 +0200

    work in progress login page

--- components/UserAuthForm.vue
+++ components/UserAuthForm.vue
@@ -1,29 +1,3 @@
-<!-- <template>
-  <div>
-    <form id="UserForm">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email:</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email outside of this test.</div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password:</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-      </div>
-      <div class="mb-4">
-        <nuxt-link to="/signup" class="text-decoration-none fw-bold">Register</nuxt-link>
-      </div>
-      <button type="submit" value="submit" class="btn btn-primary fw-bold">Submit</button>
-    </form>
-  </div>
-</template>
-
-<script>
-</script>
-
-<style>
-</style> -->
-
 <template>
   <v-main>
     <v-container fluid fill-height>
@@ -42,7 +16,7 @@
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="black" @click="loginHandler">Login</v-btn>
+              <button @click="submitForm">Login</button>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -54,28 +28,16 @@
 <script>
 
 export default {
-  name: 'Login',
   data() {
     return {
-      email: null,
-      password: null
+      showPassword: false,
+      userInfo: {
+        email: '',
+        password: ''
+      },
     }
   },
-  methods: {
-    async loginHandler() {
-      const data = { 'email': this.email, 'password': this.password }
-      console.log(data);
-      try {
-        const response = await this.$auth.loginWith('local', { data: data })
-        console.log(response)
-        this.$auth.$storage.setUniversal('email', response.data.email)
-        await this.$auth.setUserToken(response.data.access_token, response.data.refresh_token)
-
-      } catch (e) {
-        console.log(e.message)
-      }
-    }
-  }
+  props: ["submitForm"]
 };
 </script>
 
--- pages/login.vue
+++ pages/login.vue
@@ -5,19 +5,24 @@
       <h1 class="mb-5">Login</h1>
 
       <!-- Components form -->
-      <UserAuthForm />
+      <UserAuthForm :submitForm="loginUser" />
 
     </div>
   </div>
 </template>
 
 <script>
-import UserAuthForm from "~/components/UserAuthForm.vue";
-
+import UserAuthForm from "@/components/UserAuthForm";
 export default {
   components: {
     UserAuthForm
   },
+  methods: {
+    loginUser(loginInfo) {
+      debugger
+      alert('You pressed a button')
+    }
+  }
 };
 </script>
 
